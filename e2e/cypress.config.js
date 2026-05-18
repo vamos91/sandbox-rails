ac279a33909da1962fb7fc7db46874c1121f3b64
vamos91 <acardnicolas91@gmail.com>
2026-05-18 13:30:32 +0200

    rails project

--- e2e/cypress.config.js
+++ e2e/cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "http://localhost:5017",
+    baseUrl: "http://localhost:3000",
     defaultCommandTimeout: 10000,
     supportFile: "cypress/support/index.js",
   }
