fe9c047c44981efdb6653bfc883c537a83c940f1
vamos91 <acardnicolas91@gmail.com>
2026-05-18 17:59:43 +0200

    deploy

--- config/puma.rb
+++ config/puma.rb
@@ -25,18 +25,32 @@
 # Any libraries that use a connection pool or another resource pool should
 # be configured to provide at least as many connections as the number of
 # threads. This includes Active Record's `pool` parameter in `database.yml`.
-threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
-threads threads_count, threads_count
+# threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
+# threads threads_count, threads_count
 
-# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
+# # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
+# port ENV.fetch("PORT", 3000)
+
+# # Allow puma to be restarted by `bin/rails restart` command.
+# plugin :tmp_restart
+
+# # Run the Solid Queue supervisor inside of Puma for single-server deployments.
+# plugin :solid_queue if ENV["SOLID_QUEUE_IN_PUMA"]
+
+# # Specify the PID file. Defaults to tmp/pids/server.pid in development.
+# # In other environments, only set the PID file if requested.
+# pidfile ENV["PIDFILE"] if ENV["PIDFILE"]
+
+#---------
+#
+#
+max_threads_count = ENV.fetch("RAILS_MAX_THREADS", 5)
+min_threads_count = ENV.fetch("RAILS_MIN_THREADS", max_threads_count)
+
+threads min_threads_count, max_threads_count
+
 port ENV.fetch("PORT", 3000)
 
-# Allow puma to be restarted by `bin/rails restart` command.
+environment ENV.fetch("RAILS_ENV", "development")
+
 plugin :tmp_restart
-
-# Run the Solid Queue supervisor inside of Puma for single-server deployments.
-plugin :solid_queue if ENV["SOLID_QUEUE_IN_PUMA"]
-
-# Specify the PID file. Defaults to tmp/pids/server.pid in development.
-# In other environments, only set the PID file if requested.
-pidfile ENV["PIDFILE"] if ENV["PIDFILE"]
